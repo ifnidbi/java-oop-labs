9dd6a4341b28f93bf7b08d2a1d9f06bcf17c0fd9
Ates Tural <atesturalca@gmail.com>
2025-10-28 16:54:06 -0400

    Add Lab 1 - Basic Hello World program

---
+++ lab-01/MyFirstJavaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Ates Tural
+ * Course: Object-Oriented Programming (Ontario Tech University)
+ * Lab: 1
+ * Date: October 2025
+ * Description: A simple introductory Java program that prints "Hello" and "World"
+ *              on separate lines to demonstrate basic syntax and structure.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
